e4faff83e152e2f91e9c3a3491204c8e6242c35a
Matheus <stvgamer74@gmail.com>
2023-02-24 00:15:06 -0300

    fix: :rotating_light: lint ignore line

--- src/styles/themes/styled.d.ts
+++ src/styles/themes/styled.d.ts
@@ -5,5 +5,6 @@ import { defaultTheme } from './index';
 type ITheme = typeof defaultTheme;
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends ITheme {}
 }
